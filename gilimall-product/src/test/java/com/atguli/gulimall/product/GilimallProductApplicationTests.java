511fc822f6e8006c35fe47efe735bba396df7e98
hengheng <674562748@qq.com>
2020-06-26 00:33:42 +0800

    第一次提交，创建项目

---
+++ gilimall-product/src/test/java/com/atguli/gulimall/product/GilimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguli.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
